8ec2d46d99ab50ba23f241e5e0822990b54a084a
shana <shanoulefevre@orange.fr>
2021-03-10 21:26:08 +0100

    aj

--- Fenetre.java
+++ Fenetre.java
@@ -11,14 +11,21 @@ import javax.swing.*;
 
 public class Fenetre extends JFrame {
   	/**
-	* Création de la fentre et ajout du controller
+	* Création de la fenetre et ajout du controller
 	*/
     public Fenetre () {
     	super ("SameGame");
     	this.setSize (800,600);
 	  	this.setLocation (200,200);
+		this.setResizable(false);
+		//this.add(new Fond(""));
 	  	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	  
+		/**
+		 * Appel de la classe MouseFenetreControl qui permettra le controle du menu
+		 */
+		MouseFenetreControl control = new MouseFenetreControl (this);
+		this.addMouseListener(control);
   	}
 
 }
--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+# MakeFile du Projet SameGame 
+# Version Avril 2021
+# By Arthur DECORBEZ & Shana LEFEVRE
+
+
 ### VARIABLES ###
 
 JC = javac
@@ -6,13 +11,18 @@ JCFLAGS = -encoding UTF-8 -implicit:none
 JVM = java
 JVMFLAGS =
 
+
 ### REGLES ESSENTIELLES ###
 
 Main.class : Main.java Fenetre.class
 	${JC} ${JCFLAGS} Main.java
 
-Fenetre.class : Fenetre.java
+Fenetre.class : Fenetre.java MouseFenetreControl.class
 	${JC} ${JCFLAGS} Fenetre.java
+
+MouseFenetreControl.class : MouseFenetreControl.java 
+	${JC} ${JCFLAGS} MouseFenetreControl.java
+
 
 
 ### REGLES OPTIONNELLES ###
